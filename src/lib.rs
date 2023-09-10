5930fc5b48e5941ed1f84f6e64d76cac6a96cf91
Ivor Wanders <ivor@iwanders.net>
2023-09-10 15:23:08 -0400

    Support sparse landmark.

--- src/lib.rs
+++ src/lib.rs
@@ -6,29 +6,19 @@ mod mock;
 mod util;
 pub use util::Rect;
 
-pub fn main() -> Result<(), Error> {
-    let mut mock_capture = mock::MockScreenCapture::new(std::path::Path::new("/tmp/frames/"))?;
+use screen_capture::RGB;
 
-    let _frame1 = mock_capture.next_frame()?;
-    let _roi = util::Rect {
-        x: 0,
-        y: 0,
-        w: 640,
-        h: 408,
-    };
-
-    // let mut odom = FeatureOdom::new(roi);
-
-    // let reference_points = odom.get_features(&frame1);
-    // odom.set_reference(&reference_points);
-
-    // for i in 0..100 {
-    // let _frame2 = mock_capture.next_frame()?;
-    // let delta = odom.update(&frame2);
-    // println!("i: {i:?} delta: {delta:?}  pos: {:?}", odom.position());
-    // }
-
-    Ok(())
+trait ToRgb {
+    fn to_rgb(&self) -> RGB;
+}
+impl ToRgb for Rgba<u8> {
+    fn to_rgb(&self) -> RGB {
+        RGB {
+            r: self.0[0],
+            g: self.0[1],
+            b: self.0[2],
+        }
+    }
 }
 
 use image::Rgba;
@@ -38,6 +28,96 @@ fn pixel_diff_squared(a: &Rgba<u8>, b: &Rgba<u8>) -> u16 {
         .zip(b.0.iter())
         .map(|(pa, pb)| (pa.max(pb) - pa.min(pb)) as u16)
         .sum()
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Pixel {
+    pub rgb: RGB,
+    pub offset: (u32, u32),
+}
+impl Pixel {
+    pub fn difference(&self, other: &RGB) -> u16 {
+        (self.rgb.r.max(other.r) - self.rgb.r.min(other.r)) as u16
+            + (self.rgb.g.max(other.g) - self.rgb.g.min(other.g)) as u16
+            + (self.rgb.b.max(other.b) - self.rgb.b.min(other.b)) as u16
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Landmark {
+    pixels: Vec<Pixel>,
+    pixel_difference_threshold: u16,
+    pixel_sum_threshold: u16,
+    width: u32,
+    height: u32,
+}
+
+impl Landmark {
+    pub fn from_image<T: image::GenericImageView<Pixel = Rgba<u8>>>(
+        landmark: &T,
+        pixel_difference_threshold: u16,
+        pixel_sum_threshold: u16,
+    ) -> Landmark {
+        let mut pixels = vec![];
+        let width = landmark.width();
+        let height = landmark.height();
+        for ly in 0..height {
+            for lx in 0..width {
+                let p = &landmark.get_pixel(lx, ly);
+                let rgb = RGB {
+                    r: p.0[0],
+                    g: p.0[1],
+                    b: p.0[2],
+                };
+                pixels.push(Pixel {
+                    rgb,
+                    offset: (lx, ly),
+                })
+            }
+        }
+        Landmark {
+            pixels,
+            pixel_difference_threshold,
+            pixel_sum_threshold,
+            width,
+            height,
+        }
+    }
+
+    pub fn present<T: image::GenericImageView<Pixel = Rgba<u8>>>(
+        &self,
+        img: &T,
+        position: (u32, u32),
+    ) -> bool {
+        let mut sum = 0;
+        for p in self.pixels.iter() {
+            let x = position.0 + p.offset.0;
+            let y = position.1 + p.offset.1;
+            let pixel = img.get_pixel(x, y).to_rgb();
+            let d = p.difference(&pixel);
+            if d > self.pixel_difference_threshold {
+                return false;
+            }
+            sum += d;
+        }
+        sum < self.pixel_sum_threshold
+    }
+
+    pub fn pixels(&self) -> &[Pixel] {
+        &self.pixels
+    }
+    pub fn pixel_difference_threshold(&self) -> u16 {
+        self.pixel_difference_threshold
+    }
+    pub fn pixel_sum_threshold(&self) -> u16 {
+        self.pixel_sum_threshold
+    }
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+    pub fn height(&self) -> u32 {
+        self.height
+    }
 }
 
 pub fn main_landmark() -> Result<(), Error> {
@@ -50,39 +130,29 @@ pub fn main_landmark() -> Result<(), Error> {
     let image_path = std::path::PathBuf::from("../screenshots/landmark_1.png");
     let l1 = image::open(&image_path)?.to_rgba8();
 
+    let lm = Landmark::from_image(&l1, 16, 32);
+
     let block = Rect {
         x: 0,
         y: 0,
         w: 640,
         h: 408,
     };
-    let Rect { x, y, w, h } = block;
-    let block_roi = screenshot.view(x, y, w, h);
+    // let Rect { x, y, w, h } = block;
+    // let block_roi = screenshot.view(x, y, w, h);
     // let block_roi = screenshot.view(0, 0, screenshot.width(), screenshot.height());
 
-    block_roi.to_image().save("/tmp/block_image.png")?;
+    // block_roi.to_image().save("/tmp/block_image.png")?;
 
-    fn find_match(block: &RgbaSubImage, lm: &image::RgbaImage) -> u32 {
+    fn find_match(block: &image::RgbaImage, rect: &Rect, lm: &Landmark) -> u32 {
         let pixel_diff_limit = 16;
         let sum_limit = 32;
-        for y in 0..(block.height() - lm.height()) {
-            'b: for x in 0..(block.width() - lm.width()) {
-                // here, iterate through the landmark.
-                let mut sum = 0;
-                for ly in 0..lm.height() {
-                    for lx in 0..lm.width() {
-                        let b = &block.get_pixel(x + lx, y + ly);
-                        let l = &lm.get_pixel(lx, ly);
-                        let diff = pixel_diff_squared(b, l);
-                        // println!("{x},{y}, {lx},{ly} -> {diff}");
-                        if diff > pixel_diff_limit {
-                            continue 'b;
-                        }
-                        sum += diff;
-                    }
-                }
-                if sum < sum_limit {
-                    println!("Found landmark at {x}, {y} with {sum} ");
+        for y in rect.y..(rect.y + rect.h) {
+            'b: for x in rect.x..(rect.x + rect.w) {
+                let present = lm.present(block, (x, y));
+
+                if present {
+                    println!("Found landmark at {x}, {y} ");
                 }
             }
         }
@@ -91,7 +161,7 @@ pub fn main_landmark() -> Result<(), Error> {
 
     let start = std::time::Instant::now();
 
-    let best = find_match(&block_roi, &l1);
+    let best = find_match(&screenshot, &block, &lm);
     println!("best: {best:?}, took {}", start.elapsed().as_secs_f64());
 
     Ok(())
--- src/mock.rs
+++ src/mock.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 pub struct MockScreenCapture {
     files: Vec<std::path::PathBuf>,
     index: usize,
